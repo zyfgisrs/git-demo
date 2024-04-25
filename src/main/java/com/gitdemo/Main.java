e10f4880b166a41f4fa9bc066a13d1d8f145e57a
zhouyangfan <zyf050399@163.com>
2024-04-25 18:00:39 +0800

    test修改

--- src/main/java/com/gitdemo/Main.java
+++ src/main/java/com/gitdemo/Main.java
@@ -20,5 +20,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!  dev");
         System.out.println("Hello world!");
+        System.out.println("Hello world! test");
     }
 }
