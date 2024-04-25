c07bb0997440b2edc5923c428b33d73bbb0f2fc9
zhouyangfan <zyf050399@163.com>
2024-04-25 16:47:21 +0800

    首次提交

---
+++ src/main/java/com/gitdemo/Main.java
@@ -0,0 +1,18 @@
+package com.gitdemo;
+
+/**
+ * @BelongsProject: Default (Template) Project
+ * @BelongsPackage: com.gitdemo
+ * @Author: zhouyangfan
+ * @Email: zyf050399@163.com
+ * @CreateTime: 2024/4/25 16:41
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
