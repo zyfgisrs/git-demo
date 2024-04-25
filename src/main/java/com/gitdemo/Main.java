9fa524bc46f079d74ced7415df6a2ad1e0d8d3dc
zhouyangfan <zyf050399@163.com>
2024-04-25 17:04:43 +0800

    第二次提交

--- src/main/java/com/gitdemo/Main.java
+++ src/main/java/com/gitdemo/Main.java
@@ -14,5 +14,6 @@ package com.gitdemo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
